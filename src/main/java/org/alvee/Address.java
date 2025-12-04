8bd2d1fece30d24e0ff9e8b00d5dcce3a81a9e0c
Alvee <alvee.bhuiyan8@gmail.com>
2025-12-04 01:11:14 -0500

    toString, equals, getter, setter, fields and Province enum

--- src/main/java/org/alvee/Address.java
+++ src/main/java/org/alvee/Address.java
@@ -1,17 +1,20 @@
 package org.alvee;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import lombok .*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+public class Address {
+    private int streetNo;
+    private String street;
+    private String city;
+    private Province province;
+    private String postalCode;
+
+    public enum Province {
+        QC, ON, AB, BC, MB, NB, NL, NS, NT, NU, PE, SK, YT
     }
+
 }
