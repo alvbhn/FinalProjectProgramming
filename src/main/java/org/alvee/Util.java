00b472d51d054302624946f7eeced8d1cab41ccb
Alvee <alvee.bhuiyan8@gmail.com>
2025-12-06 12:56:07 -0500

    create null or empty case for Util class

--- src/main/java/org/alvee/Util.java
+++ src/main/java/org/alvee/Util.java
@@ -1,5 +1,10 @@
 package org.alvee;
 
 public class Util {
+    public static String toTitleCase(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
 
+    }
 }
