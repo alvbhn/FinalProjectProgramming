ca3b557098cfe277890c170fa304c7a027613ada
Alvee <alvee.bhuiyan8@gmail.com>
2025-12-06 11:18:56 -0500

    create the fields and static int nextId for Student class

--- src/main/java/org/alvee/Student.java
+++ src/main/java/org/alvee/Student.java
@@ -1,4 +1,12 @@
 package org.alvee;
 
 public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private List<Course> registeredCourses;
+
+    private static int nextId = 1;
 }
