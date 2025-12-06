9a1d7c2541f31d262136d4be2061c12ddd778584
Alvee <alvee.bhuiyan8@gmail.com>
2025-12-05 21:53:47 -0500

    create getter for every field, equals and toString and the fields for Department class

--- src/main/java/org/alvee/Department.java
+++ src/main/java/org/alvee/Department.java
@@ -1,4 +1,14 @@
 package org.alvee;
 
+import lombok.*;
+
+@Getter
+@EqualsAndHashCode
+@ToString
 public class Department {
+    private String departmentId;
+    private String departmentName;
+    private static int nextId = 1;
+
+
 }
