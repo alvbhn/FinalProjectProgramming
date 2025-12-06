7b195af752ed34f5be531c1256e2e2fea5bb3685
Alvee <alvee.bhuiyan8@gmail.com>
2025-12-06 12:05:40 -0500

    create the fields and static int nextId for the Assignment class

---
+++ src/main/java/org/alvee/Assignment.java
@@ -0,0 +1,12 @@
+package org.alvee;
+
+import java.util.List;
+
+public class Assignment {
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private List<Integer> scores;
+
+    private static int nextId;
+}
