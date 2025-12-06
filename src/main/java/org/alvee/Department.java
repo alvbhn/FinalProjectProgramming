6a68c93139de9c6b4db9653f7095ba415685e690
Alvee <alvee.bhuiyan8@gmail.com>
2025-12-05 23:02:41 -0500

    create constructor and setter for departmentName only

--- src/main/java/org/alvee/Department.java
+++ src/main/java/org/alvee/Department.java
@@ -7,6 +7,7 @@ import lombok.*;
 @ToString
 public class Department {
     private String departmentId;
+    @Setter
     private String departmentName;
     private static int nextId = 1;
 
@@ -27,4 +28,13 @@ public class Department {
         return true;
     }
 
+    public Department(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentId = String.format("D%02d", nextId++);
+            this.departmentName = departmentName;
+        } else {
+            this.departmentId = null;
+            this.departmentName = null;
+        }
+    }
 }
