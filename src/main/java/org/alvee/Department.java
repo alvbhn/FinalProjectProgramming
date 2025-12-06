79cff1662dbbb911742ee9eca8995e04bc82b779
Alvee <alvee.bhuiyan8@gmail.com>
2025-12-05 22:39:39 -0500

    create isDepartmentNameValid method in Department class

--- src/main/java/org/alvee/Department.java
+++ src/main/java/org/alvee/Department.java
@@ -10,5 +10,25 @@ public class Department {
     private String departmentName;
     private static int nextId = 1;
 
+    static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null || departmentName.isEmpty()) {
+            return false;
+        }
+
+        for (int i = 0; i <departmentName.length(); i++) {
+            char c = departmentName.charAt(i);
+            if (c == ' ') {
+                continue;
+            }
+            if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+                 continue;
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
 
 }
