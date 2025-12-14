6452f6ce3238186e6782431c09c3d47a380ef3af
Alvee <alvee.bhuiyan8@gmail.com>
2025-12-13 22:43:31 -0500

    add a getter for scores field in Assignment class

--- src/main/java/org/alvee/Assignment.java
+++ src/main/java/org/alvee/Assignment.java
@@ -1,11 +1,14 @@
 package org.alvee;
 
+import lombok.*;
+
 import java.util.List;
 
 public class Assignment {
     private String assignmentId;
     private String assignmentName;
     private double weight;
+    @Getter
     private List<Integer> scores;
 
     private static int nextId;
