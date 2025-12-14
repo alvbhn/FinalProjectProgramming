d9d247a339b0effc9a83e6decbfbacb9cdff3205
Alvee <alvee.bhuiyan8@gmail.com>
2025-12-14 17:11:43 -0500

    create generateRandomScore method

--- src/main/java/org/alvee/Assignment.java
+++ src/main/java/org/alvee/Assignment.java
@@ -3,6 +3,7 @@ package org.alvee;
 import lombok.*;
 
 import java.util.List;
+import java.util.Random;
 
 public class Assignment {
     private String assignmentId;
@@ -24,5 +25,28 @@ public class Assignment {
         }
 
         return sum / scores.size();
+    }
+
+    public void generateRandomScore() {
+        Random random = new Random();
+
+        for (int i = 0; i < scores.size(); i++) {
+            int r = random.nextInt(11);
+            int score;
+
+            if (r == 0) {
+                score = random.nextInt(60);
+            } else if (r == 1 || r == 2) {
+                score = 60 + random.nextInt(10);
+            } else if (r == 3 || r == 4) {
+                score = 70 + random.nextInt(10);
+            } else if (r >= 5 && r <= 8) {
+                score = 80 + random.nextInt(10);
+            } else {
+                score = 90 +random.nextInt(11);
+            }
+
+            scores.set(i, score);
+        }
     }
 }
