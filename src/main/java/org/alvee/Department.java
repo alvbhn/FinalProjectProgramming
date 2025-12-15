aa858bfd4edda849edbfb4473825ad21154b751d
Alvee <alvee.bhuiyan8@gmail.com>
2025-12-14 23:41:44 -0500

    department toTitleCase

--- src/main/java/org/alvee/Department.java
+++ src/main/java/org/alvee/Department.java
@@ -42,7 +42,7 @@ public class Department {
     public Department(String departmentName) {
         if (isDepartmentNameValid(departmentName)) {
             this.departmentId = String.format("D%02d", nextId++);
-            this.departmentName = departmentName;
+            this.departmentName = util.Util.toTitleCase(departmentName);
         } else {
             this.departmentId = null;
             this.departmentName = null;
