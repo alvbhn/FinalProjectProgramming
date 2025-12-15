8bf931b91ea523d8929cad403ea557ab60824e9a
Alvee <alvee.bhuiyan8@gmail.com>
2025-12-14 22:30:04 -0500

    create address class tests

--- src/test/java/org/alvee/AllTests.java
+++ src/test/java/org/alvee/AllTests.java
@@ -1,8 +1,36 @@
 package org.alvee;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AllTests {
+    // Address
+    @Test
+    @DisplayName("isPostalCodeValid: null -> false")
+    void isPostalCodeValid1() {
+        boolean expected = false;
+        boolean actual = Address.isPostalCodeValid(null);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("isPostalCodeValid: H3N2C4 -> true")
+    void isPostalCodeValid2() {
+        boolean expected = true;
+        boolean actual = Address.isPostalCodeValid("H3N2C4");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("isPostalCodeValid: A1B2 -> false")
+    void isPostalCodeValid3() {
+        boolean expected = false;
+        boolean actual = Address.isPostalCodeValid("A1B2");
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
