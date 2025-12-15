2274e38af4284d3c4dc174e10a1e3b0cce09c782
Alvee <alvee.bhuiyan8@gmail.com>
2025-12-14 22:45:15 -0500

    create department class tests and update tests

--- src/test/java/org/alvee/AllTests.java
+++ src/test/java/org/alvee/AllTests.java
@@ -9,7 +9,7 @@ public class AllTests {
     // Address
     @Test
     @DisplayName("isPostalCodeValid: null -> false")
-    void isPostalCodeValid1() {
+    void testIsPostalCodeValid1() {
         boolean expected = false;
         boolean actual = Address.isPostalCodeValid(null);
 
@@ -18,7 +18,7 @@ public class AllTests {
 
     @Test
     @DisplayName("isPostalCodeValid: H3N2C4 -> true")
-    void isPostalCodeValid2() {
+    void testIsPostalCodeValid2() {
         boolean expected = true;
         boolean actual = Address.isPostalCodeValid("H3N2C4");
 
@@ -27,10 +27,35 @@ public class AllTests {
 
     @Test
     @DisplayName("isPostalCodeValid: A1B2 -> false")
-    void isPostalCodeValid3() {
+    void testIsPostalCodeValid3() {
         boolean expected = false;
         boolean actual = Address.isPostalCodeValid("A1B2");
 
         Assertions.assertEquals(expected, actual);
+    }
+
+    // Department
+    @Test
+    @DisplayName("isDepartmentNameValid: null -> false")
+    void testIsDepartmentNameValid1() {
+        boolean expected = false;
+        boolean actual = Department.isDepartmentNameValid(null);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("isDepartmentNameValid: Computer Science -> true")
+    void testIsDepartmentNameValid2() {
+        boolean expected = true;
+        boolean actual = Department.isDepartmentNameValid("Computer Science");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("isDepartmentNameValid: Math101 -> false")
+    void testIsDepartmentNameValid3() {
+        boolean expected = false;
+        boolean actual = Department.isDepartmentNameValid("Math101");
+        assertEquals(expected, actual);
     }
 }
